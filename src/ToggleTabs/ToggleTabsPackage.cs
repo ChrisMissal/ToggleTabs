c692f5e295fb4acfc51ea0cefaf7e5996b965c85
maydaytx <maydaytx@gmail.com>
2012-10-11 13:31:38 -0500

    fixed toggle...the Value getter on the InsertTabs item of the AllLanguages property throws a COMException.  resorted to storing the state in a registry setting, rather than relying on VS.

--- src/ToggleTabs/ToggleTabsPackage.cs
+++ src/ToggleTabs/ToggleTabsPackage.cs
@@ -5,6 +5,7 @@ using System.ComponentModel.Design;
 using EnvDTE;
 using EnvDTE80;
 using Microsoft.VisualStudio.Shell;
+using Microsoft.Win32;
 
 namespace Headspring.ToggleTabs
 {
@@ -29,20 +30,31 @@ namespace Headspring.ToggleTabs
 
             _dte = GetGlobalService(typeof (DTE)) as DTE2;
 
-            var mcs = GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            var mcs = GetService(typeof (IMenuCommandService)) as OleMenuCommandService;
 
-            if (null != mcs)
+            if (mcs != null)
             {
                 var menuCommandId = new CommandID(GuidList.guidToggleTabsCmdSet, (int) PkgCmdIDList.cmdidToggleTabs);
-                var menuItem = new MenuCommand(MenuItemCallback, menuCommandId );
-                mcs.AddCommand( menuItem );
+                var menuItem = new MenuCommand(MenuItemCallback, menuCommandId);
+                mcs.AddCommand(menuItem);
             }
         }
 
         private void MenuItemCallback(object sender, EventArgs e)
         {
-            var currentSetting = (bool) _dte.Properties["TextEditor", "AllLanguages"].Item("InsertTabs").Value;
-            _dte.Properties["TextEditor", "AllLanguages"].Item("InsertTabs").Value = !currentSetting;
+            const string keyName = @"Software\Headspring\ToggleTabs";
+            const string subkeyName = "CurrentState";
+
+            var key = Registry.CurrentUser.OpenSubKey(keyName, true) ?? Registry.CurrentUser.CreateSubKey(keyName);
+
+            if (key == null)
+                return;
+
+            var currentState = (int) key.GetValue(subkeyName, 0);
+            currentState = currentState == 0 ? 1 : 0;
+
+            _dte.Properties["TextEditor", "AllLanguages"].Item("InsertTabs").Value = currentState;
+            key.SetValue(subkeyName, currentState, RegistryValueKind.DWord);
         }
     }
 }
